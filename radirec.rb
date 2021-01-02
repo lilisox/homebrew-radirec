2d91cea81b06c8c5d4390a6b2f4811292023d7c5
lilisox <76801081+lilisox@users.noreply.github.com>
2021-01-02 09:11:07 +0900

    1st commit

---
+++ radirec.rb
@@ -0,0 +1,11 @@
+cask "radirec" do
+  version "1.0.2"
+  sha256 "b05bd3f02c09f5ccfaf60717f40e6a53fd51c717773b3d702a7959746d610258"
+
+  url "https://dogaraag.temporarydomain.net/download/installer/radirec/mac/RadiRec.pkg"
+  name "RadiRec"
+  desc "Radiko Recorder"
+  homepage "https://dogaradi.123net.jp/dl-radirec-mac/"
+
+  pkg "RadiRec.pkg"
+end
